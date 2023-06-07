dff410e868a97f2231465cbcb3b0e197947233fe
Paul Charousset <pacharousset@gmail.com>
2023-06-07 14:32:12 +0200

    dd

--- routes/stories.routes.js
+++ routes/stories.routes.js
@@ -87,7 +87,9 @@ router.get("/user/:userId", isAuthenticated, async (req, res, next) => {
     // Find stories where the user is the creator or a contributor
     const stories = await Story.find({
       $or: [{ creator: userId }, { contributors: userId }],
-    });
+    })
+      .populate("creator", "userName")
+      .populate("contributors", "userName");
 
     // Send a JSON response with the found stories
     res.json(stories);
